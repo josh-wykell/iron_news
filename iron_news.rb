431ffb54fd892709bb8a25fece1ffb7a050b9413
Josh Wykell <jwykell@gmail.com>
2015-05-17 22:21:16 -0400

    yaml not quite working

---
+++ iron_news.rb
@@ -0,0 +1,20 @@
+require 'erb'
+require 'webrick'
+require 'yaml'
+
+ROOT = File.dirname(__FILE__)
+
+server = WEBrick::HTTPServer.new(:Port => 8000)
+
+server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
+server.mount_proc '/' do |req, res|
+  @data = YAML.load_file('iron_news.yml')
+  template = ERB.new(File.read("#{ROOT}/index.html.erb"))
+  res.body = template.result
+end
+
+trap 'INT' do
+  server.shutdown
+end
+
+server.start
